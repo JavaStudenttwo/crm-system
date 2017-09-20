fd6236b35ef52461423fcc124cf2258741713140
JavaStudenttwo <13718703372@163.com>
2017-09-20 12:07:14 +0800

    用户部分完善

--- crm-web/src/main/java/com/itheima/crm/controller/UserAction.java
+++ crm-web/src/main/java/com/itheima/crm/controller/UserAction.java
@@ -1,8 +1,16 @@
 package com.itheima.crm.controller;
 
+import com.itheima.crm.domain.ElecSystemddl;
 import com.itheima.crm.domain.ElecUser;
+import com.itheima.crm.service.IElecSystemDDLService;
+import com.itheima.crm.service.IElecUserService;
+import com.itheima.crm.service.impl.ElecSystemDDLServiceImpl;
+import com.itheima.crm.service.impl.ElecUserServiceImpl;
 import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Controller;
+
+import javax.annotation.Resource;
+import java.util.List;
 
 /**
  * Created by 13718 on 2017/9/13.
@@ -13,5 +21,28 @@ public class UserAction extends BaseAction<ElecUser>{
 
     ElecUser elecUser = this.getModel();
 
+    @Resource(name = IElecUserService.SERVICE_NAME)
+    IElecUserService elecUserService = new ElecUserServiceImpl();
 
+    @Resource(name = IElecSystemDDLService.SERVICE_NAME)
+    IElecSystemDDLService elecSystemDDLService = new ElecSystemDDLServiceImpl();
+
+    public String home(){
+
+        List<ElecSystemddl> jctList = elecSystemDDLService.findSystemDDLListByKeyword("所属单位");
+        request.setAttribute("jctList",jctList);
+        return "home";
+    }
+
+    public String edit(){
+        return "edit";
+    }
+
+    public String add(){
+        return "add";
+    }
+
+    public String search(){
+        return "search";
+    }
 }
